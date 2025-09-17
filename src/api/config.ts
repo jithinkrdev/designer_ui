4423f5338cad8e0af39dfef16891cdfee5b7e444
jithin kr <jithinkr@jithins-MacBook-Pro.local>
2025-09-17 10:17:59 +0530

    fix: changed base url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://design-ai-804698015211.asia-south1.run.app/api",
   headers: {
     "Content-Type": "application/json",
   },
